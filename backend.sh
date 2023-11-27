02188a4557e0e13166274f5828c9acc6414f012f
Rishi <marachirishwitha2000@gmail.com>
2023-11-27 16:39:52 +0530

    automation

--- backend.sh
+++ backend.sh
@@ -1,25 +1,35 @@
+echo -e "\e[31m Disable older version and enable nodejs:18 \e[0m"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
+echo -e "\e[32m Install Node.js \e[0m"
 dnf install nodejs -y
 
+echo -e "\e[33m Copying Backend Service \e[0m"
 cp backend.service /etc/systemd/system/backend.service
 
+echo -e "\e[34m Add application User \e[0m"
 useradd expense
 
+echo -e "\e[35m Making a directory with app \e[0m"
 mkdir /app
 
+echo -e "\e[36m Download the application code to created app directory \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
 cd /app
 unzip /tmp/backend.zip
 
+echo -e "\e[31m download the dependencies \e[0m"
 cd /app
 npm install
 
+echo -e "\e[32m install mysql \e[0m"
 dnf install mysql -y
 
-mysql -h 172.31.41.117 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo -e "\e[33m Load Schema \e[0m"
+mysql -h 3.81.37.30 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
+echo -e "\e[34m Reload Enable and Restart \e[0m"
 systemctl daemon-reload
 systemctl enable backend
 systemctl restart backend
