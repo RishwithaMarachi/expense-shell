c018d8ef17131645c7a46bd8d1133f6543bc8136
Rishi <marachirishwitha2000@gmail.com>
2023-11-29 09:13:35 +0530

    automation

--- backend.sh
+++ backend.sh
@@ -36,11 +36,11 @@ fi
 echo -e "$COLOR Add application User \e[0m"
 id expense &>>$log_file
 if [ $? -ne 0 ]; then
- useradd expense &>>$log_file
- if [ $? -eq 0 ]; then
-  echo -e "\e[32m SUCCESS \e[0m"
- else
-  echo -e "\e[31m FAILURE \e[0m"
+  useradd expense &>>$log_file
+  if [ $? -eq 0 ]; then
+   echo -e "\e[32m SUCCESS \e[0m"
+  else
+   echo -e "\e[31m FAILURE \e[0m"
  fi
 fi
 
